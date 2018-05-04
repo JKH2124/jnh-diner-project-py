b4d29445134fbbcb3c16ac12464fb43f1af29482
Joshua Harris <jnharris208@gmail.com>
2018-05-04 13:06:21 -0600

    Added variables and an if statement.

---
+++ diner_project.py
@@ -0,0 +1,13 @@
+# J & K's Diner
+
+# J & K's Diner
+
+dinnerMenu = ['Steak', 'Chicken', 'Pork', 'Salad']
+sidesMenu = ['Fries', 'Rice', 'Veggies', 'Soup']
+
+dinnerSelect = input("Please select an entree: ")
+
+if dinnerSelect == ().index(0):
+  print("Excellent Choice")
+else: 
+  print("Please make a valid selection")
